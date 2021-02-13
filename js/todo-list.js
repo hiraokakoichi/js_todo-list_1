cfbce4c097ad3023ea5ee15a1b90db69b5e022dd
hiraokakoichi <the10pointsflavor0327@gmail.com>
2021-02-14 01:00:14 +0900

    オブジェクトで管理

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,13 +1,12 @@
 let task_id = 1;
+let todos = [];
 
-const add_btn = document.getElementById("add_btn");
+const add_btn = document.getElementById('add_btn');
 
-add_btn.addEventListener('click', function() {
-    // 入力されたタスクを取得
-    let task = document.getElementById("task").value;
-    
+function displayTodos(todos){
     // タスク追加先のtable要素を取得
-    let task_table = document.getElementById("task_table_body");
+    let task_table = document.getElementById('task_table_body');
+
     /***** 1列目 *****/
     // テーブルに行を追加
     let new_row = task_table.insertRow();
@@ -20,21 +19,41 @@ add_btn.addEventListener('click', function() {
 
     /***** 2列目 *****/
     new_cell = new_row.insertCell();
-    new_text = document.createTextNode(task);
+    new_text = document.createTextNode(todos[todos.length-1]['task']);
     new_cell.appendChild(new_text);
     task_id++;
 
     /***** 3列目 *****/
-    let status_btn = document.createElement("BUTTON");
-    status_btn.textContent = "作業中";
+    let status_btn = document.createElement('BUTTON');
+    status_btn.textContent = todos[todos.length-1]['status'];
     new_row.appendChild(status_btn);
 
     /***** 4列目 *****/
-    let remove_btn = document.createElement("BUTTON");
-    remove_btn.textContent = "削除";
+    let remove_btn = document.createElement('BUTTON');
+    remove_btn.textContent = '削除';
     new_row.appendChild(remove_btn);
 
-    // タスク入力ボックスをクリア
-    document.getElementById("task").value = "";
+}
 
+add_btn.addEventListener('click', function() {
+    // 入力されたタスクを取得
+    let taskText = document.getElementById('task').value;
+
+    if( taskText.length > 0 ){
+        
+        let todo = {
+            task : taskText,
+            status : '作業中'
+
+        };
+
+        console.log(taskText);
+
+        todos.push(todo);
+
+        displayTodos(todos);
+        
+        // タスク入力ボックスをクリア
+        document.getElementById('task').value = '';
+    }
 });
