85719428be63cad4200ff0dbb1816bea35e1c414
hiraokakoichi <the10pointsflavor0327@gmail.com>
2021-02-16 01:20:58 +0900

    ToDoの追加、表示、ボタン生成関数を追記

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,11 +1,30 @@
 let task_id = 1;
-let todos = [];
+let todoList = [];
 
 const add_btn = document.getElementById('add_btn');
 
-function displayTodos(todos){
+
+function addTodo(){
+    // 入力されたタスクを取得
+    const taskText = document.getElementById('task').value;
+
+    // バリデーション
+    if( taskText.length > 0 ){
+        const todo = {
+            task : taskText,
+            status : '作業中' // 初期値
+        };
+        todoList.push(todo);
+        
+        // タスク入力ボックスをクリア
+        document.getElementById('task').value = '';
+    }
+
+}
+
+function showTodoList(){
     // タスク追加先のtable要素を取得
-    let task_table = document.getElementById('task_table_body');
+    const task_table = document.getElementById('task_table_body');
 
     /***** 1列目 *****/
     // テーブルに行を追加
@@ -16,44 +35,39 @@ function displayTodos(todos){
     let new_text = document.createTextNode(task_id);
     // セルにテキストノードを追加
     new_cell.appendChild(new_text);
+    task_id++;
 
     /***** 2列目 *****/
     new_cell = new_row.insertCell();
-    new_text = document.createTextNode(todos[todos.length-1]['task']);
+    new_text = document.createTextNode(todoList[todoList.length-1]['task']);
     new_cell.appendChild(new_text);
-    task_id++;
+}
 
-    /***** 3列目 *****/
-    let status_btn = document.createElement('BUTTON');
-    status_btn.textContent = todos[todos.length-1]['status'];
-    new_row.appendChild(status_btn);
-
-    /***** 4列目 *****/
-    let remove_btn = document.createElement('BUTTON');
-    remove_btn.textContent = '削除';
-    new_row.appendChild(remove_btn);
-
+function makeBtn(btnLabel){
+    // タスク追加先のtable要素を取得
+    const task_table = document.getElementById('task_table_body');
+    // 最後の行を取得
+    const rowLength = task_table.rows.length;
+    let last_row = task_table.rows[rowLength-1];
+    // 行にセルを追加
+    let new_cell = last_row.insertCell();
+    // ボタン要素生成
+    let status_btn = document.createElement('button');
+    status_btn.textContent = btnLabel;
+    new_cell.appendChild(status_btn);
+    
 }
 
 add_btn.addEventListener('click', function() {
-    // 入力されたタスクを取得
-    let taskText = document.getElementById('task').value;
+    // todoを追加
+    addTodo();
 
-    if( taskText.length > 0 ){
-        
-        let todo = {
-            task : taskText,
-            status : '作業中'
+    // todoを表示
+    showTodoList();
 
-        };
+    // 状態ボタンを表示
+    makeBtn(todoList[todoList.length-1]['status']);
 
-        console.log(taskText);
-
-        todos.push(todo);
-
-        displayTodos(todos);
-        
-        // タスク入力ボックスをクリア
-        document.getElementById('task').value = '';
-    }
+    // 削除ボタンを表示
+    makeBtn('削除');
 });
